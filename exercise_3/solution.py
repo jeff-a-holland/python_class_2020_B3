4404dadb8049d3448b8c1c15b4a84f1191edcdde
JEFF HOLLAND <jeff@jeffs-mbp.lan>
2020-11-12 11:44:00 -0600

    Updates

--- exercise_3/solution.py
+++ exercise_3/solution.py
@@ -5,40 +5,58 @@ import tarfile
 import re
 import os
 
-def tar_to_zip(*args, **zippath):
-    zippath = './'
-    extraction_dir = '/tmp'
-    tmp_dir_file_before = os.listdir(extraction_dir)
-    print(f'before: {tmp_dir_file_before}')
+def tar_to_zip(*inputfiles, **zip_path):
 
-    for arg in args:
-        if arg.startswith('zippath'):
-            zippath = re.sub('^.*?=', '', arg) + '/'
-            print (f'\nzippath is: {zippath}')
+    for path in zip_path.values():
+        zippath = path
+        print (f'\nzippath is: {zippath}')
 
-        elif tarfile.is_tarfile(arg):
-            print (f'\n{arg} is a tarfile. Proceeding to untar it...')
-            tf = tarfile.open(arg, 'r')
-            tf.extractall(extraction_dir)
+    file_status = str(os.path.exists(zippath))
+    if file_status == 'False':
+        print('Creating dir')
+        os.mkdir(str(zippath), 0o666)
+    else:
+        print('Dir already exists')
+
+    for inputfile in inputfiles:
+        if tarfile.is_tarfile(inputfile):
+            print (f'\n{inputfile} is a tarfile. Proceeding to untar it...')
+            tf = tarfile.open(inputfile, 'r')
+            tf.extractall('./')
+
+            file_path_str = str(inputfile)
+            path_list = file_path_str.split('/')
+            tar_filename = path_list.pop()
+            print(f'\ntar\'d filename is: {tar_filename}')
+            tar_root = re.sub('\\.tar', '', tar_filename)
+            print(f'\nfilename after removing extension is: {tar_root}')
+
+            file_full_path_list = []
+            files = os.listdir(zippath)
+            print(files)
+
+            for file in files:
+                if not file.startswith('.') and not file.endswith('.tar'):
+                    print (file)
+                    file_full_path_list.append(str(zippath) + '/' + file)
+            print(f'file_full_path_list is: {file_full_path_list}')
+
+            untard_file_list = []
+            for file in file_full_path_list:
+                if not file.endswith('.tar'):
+                    untard_file_list.append(file)
+            print(f'untard_file_list: {untard_file_list}')
+            path_to_write_to = str(zippath) + '/' + tar_root + '.zip'
+            print (f'path_to_write_to is: {path_to_write_to}')
+            with ZipFile(f'{path_to_write_to}', 'w') as zipped:
+                for file in untard_file_list:
+                    print(f'file that should be zipped: {file}')
+                    filename = file
+                    filename = re.sub('^.*/', '', filename)
+                    print (f'filename is: {filename}')
+                    zipped.write(file, filename)
 
         else:
-            print(f'\nERROR. {arg} is NOT a tarfile. Skipping this file...')
+            print(f'\nERROR. {inputfile} is NOT a tarfile. Skipping this file...')
 
-    tmp_dir_file_after = os.listdir(extraction_dir)
-    print(f'after: {tmp_dir_file_after}')
-    for file in tmp_dir_file_after:
-        if file not in tmp_dir_file_before and not file.startswith('._'):
-            print(f'file that should be zipped: {file}')
-            file2 = re.sub('\..*', '', file)
-            zipped_file = zippath + file2 + '.zip'
-            print(f'zipped_file path/name is: {zipped_file}')
-            with ZipFile(zipped_file, 'w') as zipped:
-                zipped.write(zipped_file)
-                untard_file1 = extraction_dir + '/' + file
-                untard_file2 = extraction_dir + '/' + '._' + file
-                os.remove(untard_file1)
-                os.remove(untard_file2)
-
-
-#tar_to_zip('text.tar', 'tard_and_zipd.tar', 'tard_zipd_compressed.tar.gz')
-#tar_to_zip('text.tar', 'tard_and_zipd.tar', 'tard_zipd_compressed.tar.gz', 'zippath=/tmp')
+#tar_to_zip('test.tar', zippath='./temp')
