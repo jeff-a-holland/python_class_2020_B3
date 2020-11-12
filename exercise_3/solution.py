677b722aae718f005337629a303ff7d98ea3bdaa
JEFF HOLLAND <jeff@jeffs-mbp.lan>
2020-11-12 17:49:11 -0600

    Pytest results screenshot and final code

--- exercise_3/solution.py
+++ exercise_3/solution.py
@@ -8,10 +8,13 @@ import shutil
 
 def tar_to_zip(*inputfiles, **zip_path):
 
+    # Get path to create output zip files in from named kwarg
     for path in zip_path.values():
         zippath = path
         print (f'\nzippath named argument is: {zippath}')
 
+    # Determine if directory where we'll write zip files to exists.
+    # If not, create it and chmod it 777.
     file_status = str(os.path.exists(zippath))
     if file_status == 'False':
         print(f'\n  {zippath} does not exist. Creating directory...')
@@ -26,19 +29,25 @@ def tar_to_zip(*inputfiles, **zip_path):
             tf = tarfile.open(inputfile, 'r')
             tf.extractall(zippath)
 
+            # We'll name our zip file based on the root of the tarball name.
+            # Parse out the root of the name to "tar_root".
             file_path_str = str(inputfile)
             path_list = file_path_str.split('/')
             tar_filename = path_list.pop()
             tar_root = re.sub('\\.tar', '', tar_filename)
 
+            # Ignore "dot" files, such as "._" created by OSX.
+            # Also ignore .tar files for our .zip file, as well as previous
+            # .zip files if more than one file was supplied to the function.
             file_full_path_list = []
             files = os.listdir(zippath)
-
             for file in files:
                 if not file.startswith('.') and not file.endswith('.tar') and\
                    not file.endswith('.zip'):
                     file_full_path_list.append(str(zippath) + '/' + file)
 
+            # Build list of file untar'd that we'll zip up. Then create the
+            # .zip that contains all the same files from the tarball.
             untard_file_list = []
             for file in file_full_path_list:
                 if not file.endswith('.tar'):
@@ -50,6 +59,9 @@ def tar_to_zip(*inputfiles, **zip_path):
                     filename = re.sub('^.*/', '', filename)
                     zipped.write(file, filename)
 
+            # CLean up the directory where we untar'd the tarball, except
+            # for the .zip file we just created. Make sure to delete both
+            # files and any directories recursively.
             files = os.listdir(zippath)
             for file in files:
                 if not file.endswith('.zip'):
