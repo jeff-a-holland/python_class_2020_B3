a7633e40b5c368df4c76b83948731f58aa9ff209
JEFF HOLLAND <jeff@jeffs-mbp.lan>
2020-11-12 17:37:08 -0600

    Updating code and test files in .tar format

--- exercise_3/solution.py
+++ exercise_3/solution.py
@@ -4,60 +4,67 @@ from zipfile import ZipFile
 import tarfile
 import re
 import os
+import shutil
 
 def tar_to_zip(*inputfiles, **zip_path):
 
     for path in zip_path.values():
         zippath = path
-        print (f'\nzippath is: {zippath}')
+        print (f'\nzippath named argument is: {zippath}')
 
     file_status = str(os.path.exists(zippath))
     if file_status == 'False':
-        print('Creating dir')
+        print(f'\n  {zippath} does not exist. Creating directory...')
         os.mkdir(str(zippath))
         os.chmod(str(zippath), 0o777)
     else:
-        print('Dir already exists')
+        print(f'\n  {zippath} directory already exists. Continuing...')
 
     for inputfile in inputfiles:
         if tarfile.is_tarfile(inputfile):
-            print (f'\n{inputfile} is a tarfile. Proceeding to untar it...')
+            print (f'\n      {inputfile} is a tarfile. Proceeding to untar it...')
             tf = tarfile.open(inputfile, 'r')
             tf.extractall(zippath)
 
             file_path_str = str(inputfile)
             path_list = file_path_str.split('/')
             tar_filename = path_list.pop()
-            print(f'\ntar\'d filename is: {tar_filename}')
             tar_root = re.sub('\\.tar', '', tar_filename)
-            print(f'\nfilename after removing extension is: {tar_root}')
 
             file_full_path_list = []
             files = os.listdir(zippath)
-            print(files)
 
             for file in files:
-                if not file.startswith('.') and not file.endswith('.tar'):
-                    print (file)
+                if not file.startswith('.') and not file.endswith('.tar') and\
+                   not file.endswith('.zip'):
                     file_full_path_list.append(str(zippath) + '/' + file)
-            print(f'file_full_path_list is: {file_full_path_list}')
 
             untard_file_list = []
             for file in file_full_path_list:
                 if not file.endswith('.tar'):
                     untard_file_list.append(file)
-            print(f'untard_file_list: {untard_file_list}')
             path_to_write_to = str(zippath) + '/' + tar_root + '.zip'
-            print (f'path_to_write_to is: {path_to_write_to}')
             with ZipFile(f'{path_to_write_to}', 'w') as zipped:
                 for file in untard_file_list:
-                    print(f'file that should be zipped: {file}')
                     filename = file
                     filename = re.sub('^.*/', '', filename)
-                    print (f'filename is: {filename}')
                     zipped.write(file, filename)
 
-        else:
-            print(f'\nERROR. {inputfile} is NOT a tarfile. Skipping this file...')
+            files = os.listdir(zippath)
+            for file in files:
+                if not file.endswith('.zip'):
+                    filepath = str(zippath) + '/' + file
+                    if os.path.isdir(filepath):
+                        shutil.rmtree(str(filepath))
+                    elif os.path.isfile(filepath):
+                        os.remove(filepath)
 
-tar_to_zip('test.tar', zippath='./temp')
+        else:
+            print(f'\n      ERROR. {inputfile} is NOT a tarfile. Skipping this file...')
+
+    print (f'\nDone. See your zip files in the directory: {zippath}\n')
+
+#tar_to_zip('test.tar', zippath='./temp')
+#tar_to_zip('test.tar', 'test2.tar', zippath='./temp')
+#tar_to_zip('test.tar', 'test2.tar', zippath='/tmp/test')
+tar_to_zip('test.tar', 'test2.tar', 'test3.tar', zippath='/tmp/test')
