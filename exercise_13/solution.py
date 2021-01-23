34d37b05e6d6dd4221cf4dc0678736acadecca2c
JEFF HOLLAND <jeff@JEFFs-MacBook-Pro.local>
2021-01-23 09:40:58 -0600

    Update

--- exercise_13/solution.py
+++ exercise_13/solution.py
@@ -10,17 +10,20 @@ def main():
     arguments supplied in a tuple from the command line."""
 
     #def add(q):
-    def add(tuple):
+    def add(q, tuple):
         """Add function. Simply return the sumer of values in a tuple passed
         as the argument."""
         sum = 0
-        #while True:
-            #tuple = q.get()
-        for value in tuple:
-            sum += value
-        print(f'sum is: {sum}\n')
-        result_list.append(sum)
-            #q.task_done()
+        while True:
+            tuple = q.get()
+            tuple = tuple[1]
+            #print(f'tuple is: {tuple}')
+            for value in tuple:
+                sum += value
+            #print(f'sum is: {sum}\n')
+            result_list.append(sum)
+            q.task_done()
+            sum = 0
 
     def threadify(func, arg_list):
         """Threadify function. Calls the add function x times, where is the
@@ -28,31 +31,25 @@ def main():
         argument (the function to call being the first argument). Threadify
         will run the add computations in parallel using a PriorityQueue."""
 
+        q = PriorityQueue()
+        for index, value in enumerate(arg_list, start=1):
+            q.put((index, value))
+
         num_threads = len(arg_list)
-        print(F'num_threads is: {num_threads}\n')
+        #print(f'num_threads is: {num_threads}\n')
         for x in range(num_threads):
-            print(f'x is: {x}')
-            print(f'arg_list[x] is: {arg_list[x]}')
-            worker = Thread(target=add, args=(arg_list[x],))
+            #print(f'x is: {x}')
+            #print(f'arg_list[x] is: {arg_list[x]}')
+            worker = Thread(target=add, args=(q, arg_list[x],))
             worker.setDaemon(True)
             worker.start()
-
-        # result_list = []
-        # for tuple in arg_list:
-        #     result = func_dict.get(func)(tuple)
-        #     result_list.append(result)
-        # print(f'  Results from threadify function was: {result_list}\n')
-        # print(result_list)
 
     func_dict = {'add':add}
     arg_list = [(1,2), (3,4), (5,6)]
 
-    q = PriorityQueue()
-    for index, value in enumerate(arg_list, start=1):
-        q.put((index, value))
-
     threadify(add, arg_list)
-    print(f'result_list is: {result_list}')
+    #print(f'result_list is: {result_list}')
+    print(result_list)
 
 if __name__ == '__main__':
    main()
